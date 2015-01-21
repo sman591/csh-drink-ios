2566c29daa2158e64024b9f4cc418122114370aa
Stuart Olivera <stuart@stuartolivera.com>
2015-01-20 20:12:43 -0500

    Initial Commit

---
+++ CSH Drink/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CSH Drink
+//
+//  Created by Stuart Olivera on 1/20/15.
+//  Copyright (c) 2015 Stuart Olivera. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
