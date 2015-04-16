bc02816659822f4785e2f1998ec241f864df72dc
Stuart Olivera <stuart@stuartolivera.com>
2015-04-16 02:15:48 -0400

    Improved drop tab bar icon

---
+++ CSH Drink/DropNavigationController.swift
@@ -0,0 +1,27 @@
+//
+//  DropNavigationController.swift
+//  CSH Drink
+//
+//  Created by Stuart Olivera on 4/16/15.
+//  Copyright (c) 2015 Stuart Olivera. All rights reserved.
+//
+
+import UIKit
+
+class DropNavigationController: UINavigationController {
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.tabBarItem.image = UIImage(named: "drink-outline")
+        self.tabBarItem.selectedImage = UIImage(named: "drink")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
---
+++ CSH Drink/UIColor+Drink.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Drink.swift
+//  CSH Drink
+//
+//  Created by Stuart Olivera on 4/15/15.
+//  Copyright (c) 2015 Stuart Olivera. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class func drinkPinkColor() -> UIColor {
+        return UIColor(red: 0.906, green: 0.243, blue: 0.478, alpha: 1.0)
+    }
+}
