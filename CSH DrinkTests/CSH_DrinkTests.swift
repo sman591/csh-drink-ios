e160922ab0fa5e9ea10f7b27c903ab60d1718d3e
Stuart Olivera <stuart@stuartolivera.com>
2016-03-29 11:27:39 -0400

    Added fastlane testing

--- CSH DrinkTests/CSH_DrinkTests.swift
+++ CSH DrinkTests/CSH_DrinkTests.swift
@@ -2,11 +2,10 @@
 //  CSH_DrinkTests.swift
 //  CSH DrinkTests
 //
-//  Created by Stuart Olivera on 1/20/15.
-//  Copyright (c) 2015 Stuart Olivera. All rights reserved.
+//  Created by Stuart Olivera on 3/29/16.
+//  Copyright © 2016 Stuart Olivera. All rights reserved.
 //
 
-import UIKit
 import XCTest
 
 class CSH_DrinkTests: XCTestCase {
@@ -23,12 +22,13 @@ class CSH_DrinkTests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssert(true, "Pass")
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measureBlock {
             // Put the code you want to measure the time of here.
         }
     }
