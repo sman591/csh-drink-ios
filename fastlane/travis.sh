0b8805c71be12da55d9e0269791e046f53e30427
Stuart Olivera <stuart@stuartolivera.com>
2016-03-29 14:24:49 -0400

    WIP

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]] || [[ "$TRAVIS_BRANCH" == "master" ]]; then
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   fastlane test
+  exit $?
+fi
+
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+  # Travis CI fetches a shallow clone. We use commit count until HEAD for build number. In order to assure that the count is correct we have to unshallow the clone.
+  git fetch --unshallow
+
+  fastlane beta
   exit $?
 fi
